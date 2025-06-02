9728fb6eb8defff0264ba264aab2062365707428
devops979 <devops979@gmail.com>
2025-06-02 12:53:43 +0000

    default variables  & tflint.hcl file added & gh actions workflow added"

--- variables.tf
+++ variables.tf
@@ -13,13 +13,13 @@ variable "cidr_block" {
 variable "public_subnet_cidrs" {
   description = "List of CIDR blocks for public subnets"
   type        = list(string)
-  default     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  default     = ["10.0.1.0/24", "10.0.2.0/24"]
 }
 
 variable "private_subnet_cidrs" {
   description = "List of CIDR blocks for private subnets"
   type        = list(string)
-  default     = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+  default     = ["10.0.101.0/24", "10.0.102.0/24"]
 }
 
 variable "availability_zones" {
@@ -28,6 +28,12 @@ variable "availability_zones" {
   default     = ["us-east-1a", "us-east-1b", "us-east-1c"]
 }
 
+
+variable "key_name" {
+    description = "The key name of the instance"
+  type        = string
+  default     = "devops-tools"
+}
 
 variable "ami_id" {
   description = "AMI ID for the EC2 instances"
